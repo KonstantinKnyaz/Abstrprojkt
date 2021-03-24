c775a7c0af8dac0eb715a454ef184058d3c177e6
Константин Князев <79729833+KonstantinKnyaz@users.noreply.github.com>
2021-03-24 13:28:51 +0300

    Добавьте файлы проекта.

---
+++ Abstrprojkt/Abstrprojkt.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+// class Humans {
+// private:
+// 	int age;
+// public:
+// 	virtual void talk() = 0;
+// };
+
+class IHumans {
+public:
+	virtual void talk() = 0;
+};
+
+void IHumans::talk() {
+	cout << "неизвестно" << endl;
+}
+
+class People : public IHumans {
+public:
+	void talk() override {
+		IHumans::talk();
+		cout << "Привет всем!" << endl;
+	}
+};
+
+class Ufo : public IHumans {
+public:
+	void talk() override {
+		cout << "Some call" << endl;
+	}
+};
+
+class Dog : public IHumans {
+public:
+	void talk() override {
+		cout << "Gav gav" << endl;
+	}
+};
+
+int main() {
+	setlocale(LC_CTYPE, "rus");
+
+	People bob;
+	bob.talk();
+	
+	return 0;
+}
